5eec28e8fcad7d7f9700c57dbe169bae367d0a2e
Shu Foujita <osorubeki.fujita@gmail.com>
2015-04-22 14:43:36 +0900

    20150422_1445 Bug Fix - settings of required files

--- lib/tokyo_metro/required/all/static/except_for_train_type.rb
+++ lib/tokyo_metro/required/all/static/except_for_train_type.rb
@@ -3,6 +3,10 @@ class TokyoMetro::Required::All::Static::ExceptForTrainType < TokyoMetro::Requir
   def initialize
     super( set_all_files_under_the_top_namespace: false )
   end
+  
+  def self.top_file
+    ::TokyoMetro::Required::All::Static.top_file
+  end
 
   def self.other_files
     namespaces.map { | namespace |
