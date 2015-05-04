f30ac66ef3ce89a591a0d51e2eee82c3dbc9f91f
Shu Fujita <osorubeki.fujita@gmail.com>
2015-05-04 16:02:32 +0900

    20150504_160220 Bug fix of Font Awesome settings

---
+++ lib/tokyo_metro/app/renderer/rss.rb
@@ -0,0 +1,21 @@
+
+module TokyoMetro::App::Renderer::Rss
+
+  LIST = ::YAML.load_file( "#{ ::TokyoMetro::DICTIONARY_DIR }/view/rss.yaml")
+
+  def self.test
+    feed_urls = LIST.values.map { | item | item[ "url" ] }
+    feed_urls.each do | feed_url |
+      feed = ::Feedjira::Feed.fetch_and_parse(feed_urls.first)
+      
+      p feed.title          # => "PILOG"
+      p feed.url            # => "http://xoyip.hatenablog.com/"
+      p feed.feed_url       # => "http://xoyip.hatenablog.com/feed"
+      p feed.etag           # => "f6ce826cbbd07e222b6cee445fc981f730ad0693"
+      p feed.last_modified  # => 2014-01-15 11:47:34 UTC
+      p feed.entries.count  # => 7
+      p ""
+    end
+  end
+
+end
