9930bc6c7b136968196bfe9b41b7faa9a4f98ce5
Shu Fujita <osorubeki.fujita@gmail.com>
2015-05-07 19:37:36 +0900

    20150507_193732 Bug fix

--- lib/tokyo_metro/factory/convert/patch/for_string/barrier_free_facility/info/remark.rb
+++ lib/tokyo_metro/factory/convert/patch/for_string/barrier_free_facility/info/remark.rb
@@ -1,6 +1,9 @@
 module TokyoMetro::Factory::Convert::Patch::ForString::BarrierFreeFacility::Info::Remark
 
   def process
+    available = "利用時間"
+    # available = "利用可能時間"
+
     str = self.zen_num_to_han
     str = str.gsub( /。([\(（].+?[\)）])/ ) { "#{$1}。" }
     str = str.gsub( /(?<=。)\n?[ 　]?/ , "\n" )
@@ -11,10 +14,12 @@ module TokyoMetro::Factory::Convert::Patch::ForString::BarrierFreeFacility::Info
     str = str.gsub( /ご?利用/ , "ご利用" )
     str = str.gsub( /お?乗り?換え?/ , "お乗り換え" )
     str = str.gsub( /(?<=（時間制限)：?(?=\d{1,2}時～\d{1,2}時）)/ , "：" )
-    str = str.gsub( "時間制限" , "利用可能時間" )
-    str = str.gsub( /(?<=が、利用可能時間)(?=があります。)/ , "に制限" )
+    str = str.gsub( "時間制限" , available )
+    str = str.gsub( /(?<=が、#{ available })(?=があります。)/ , "に制限" )
 
-    str = str.gsub( /(利用時間に制限があります。)\n?((?:[A-Z]?(?:\d{1,2})?[a-z]?出入口（\d{1,2}時～\d{1,2}時）)+)/ ) {
+    str = str.gsub( "B8出入口エレベーターB7出入口エレベーター" , "B8出入口エレベーター、B7出入口エレベーター" )
+
+    str = str.gsub( /(#{ available }に制限があります。)\n?((?:[A-Z]?(?:\d{1,2})?[a-z]?出入口（\d{1,2}時～\d{1,2}時）)+)/ ) {
       str_ary = ::Array.new
       str_ary << $1
       service_times = $2.split( /(?<=時）)/ ).map { | s |
