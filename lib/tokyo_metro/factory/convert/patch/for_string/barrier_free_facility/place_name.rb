5e8671c63a917ca393e5f39a041fc0e6e14e539c
Shu Fujita <osorubeki.fujita@gmail.com>
2015-05-07 19:59:04 +0900

    20150507_195849 Add string replacement patch

--- lib/tokyo_metro/factory/convert/patch/for_string/barrier_free_facility/place_name.rb
+++ lib/tokyo_metro/factory/convert/patch/for_string/barrier_free_facility/place_name.rb
@@ -5,6 +5,7 @@ module TokyoMetro::Factory::Convert::Patch::ForString::BarrierFreeFacility::Plac
     str = str.convert_comma_between_number_to_dot
     str = str.gsub( "東部スカイツリーライン" , "東武スカイツリーライン" )
     str = str.gsub( /(?<=出) ?(?=入口)/ , "" )
+    str = str.gsub( /[ 　]?（([a-zA-z\d]+)）/ ) { " (#{ $1 })" }
     str
   end
 
