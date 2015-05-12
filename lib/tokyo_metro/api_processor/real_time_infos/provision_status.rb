3a4922ba47de144ec074de584dfeac3754b24cce
Shu Fujita <osorubeki.fujita@gmail.com>
2015-05-13 00:56:10 +0900

    20150513_005609 Install Gem (0.1.20)

--- lib/tokyo_metro/api_processor/real_time_infos/provision_status.rb
+++ lib/tokyo_metro/api_processor/real_time_infos/provision_status.rb
@@ -13,7 +13,7 @@ class TokyoMetro::ApiProcessor::RealTimeInfos::ProvisionStatus::EachRailwayLine
   end
 
   def train_informations_are_not_provided?
-
+  !( train_informations_are_provided? )
   end
 
   def train_locations_provided?
@@ -29,3 +29,22 @@ class TokyoMetro::ApiProcessor::RealTimeInfos::ProvisionStatus::EachRailwayLine
   end
 
 end
+
+__END__
+
+運行中の列車 なし
+No train is operated now.
+折返し運転
+本日の運行は終了しました。
+Shuttle service is provided in section with no effect by accidents.
+Today's operation was finished.
+
+
+運行中の列車 なし
+No train is operated now.
+ダイヤ乱れ
+本日の運行は終了しました。
+Trains are behind schedule.
+Today's operation was finished.
+
+東急田園都市線内での東急線内遅延により、一部の列車に遅れが出ています。
