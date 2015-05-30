967f2fcb2f156881765a94ed6966688143544635
Shu Fujita <osorubeki.fujita@gmail.com>
2015-05-31 07:07:02 +0900

    20150531_070701 Install Gem (0.3.6)

--- spec/tokyo_metro_spec.rb
+++ spec/tokyo_metro_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 require 'deplo'
 
 spec_filename = ::File.expand_path( ::File.dirname( __FILE__ ) )
-version = "0.3.5"
+version = "0.3.6"
 
 describe TokyoMetro do
   it "has a version number \'#{ version }\'" do
