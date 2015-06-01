9e387ca5227513808c4ac660751d0cfaae7a2704
Shu Fujita <osorubeki.fujita@gmail.com>
2015-06-01 21:13:26 +0900

    20150601_211251 Make spec file for surrounding_areas in platform infos of station_facility

--- spec/spec_helper/station_facility/surrounding_area.rb
+++ spec/spec_helper/station_facility/surrounding_area.rb
@@ -3,22 +3,19 @@
 # @note 正：虎の門病院
 def station_facility_surrounding_area
   dict = TokyoMetro::Modules::Api::Convert::Patches::StationFacility::SurroundingArea::Generate::Info::Platform::Info::SurroundingArea::DICTIONARY
+  
+  surrounding_area_infos =::TokyoMetro::Api.station_facilities.map( &:platform_infos ).flatten.map( &:surrounding_areas ).flatten.select( &:present? )
 
-  platform_info_containing_invalid_surrounding_area = ::TokyoMetro::Api.station_facilities.map( &:platform_infos ).flatten.select { | info |
-    dict.keys.include?( info.surrounding_area.area_name )
-  }
-
-  platform_info_containing_valid_surrounding_area = ::TokyoMetro::Api.station_facilities.map( &:platform_infos ).flatten.select { | info |
-    dict.values.include?( info.surrounding_area.area_name )
-  }
+  platform_infos_containing_invalid_surrounding_area = surrounding_area_infos.select { | info | dict.keys.include?( info.area_name ) }
+  platform_infos_containing_valid_surrounding_area = surrounding_area_infos.select { | info | dict.values.include?( info.area_name ) }
 
   describe ::TokyoMetro::Api::StationFacility::List , "after processing invalid surrounding area" do
     describe ::TokyoMetro::Api::StationFacility::Info do
       describe ::TokyoMetro::Api::StationFacility::Info::Platform::Info::SurroundingArea::Info do
         it "is replaced by valid surrounding area name." do
 
-          expect( platform_info_containing_invalid_surrounding_area ).not_to be_present
-          expect( platform_info_containing_valid_surrounding_area ).to be_present
+          expect( platform_infos_containing_invalid_surrounding_area ).not_to be_present
+          expect( platform_infos_containing_valid_surrounding_area ).to be_present
 
         end
       end
