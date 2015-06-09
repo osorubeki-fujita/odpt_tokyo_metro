5b516fb029ade261217c4ef8afa90c40b7319669
Shu Fujita <osorubeki.fujita@gmail.com>
2015-06-09 22:53:52 +0900

    20150609_225342 Bug fix - TokyoMetro::App::Renderer::RealTimeInfos::LinkToRailwayLinePage::TrainLocation

--- lib/tokyo_metro/app/renderer/real_time_infos/link_to_railway_line_page/train_location.rb
+++ lib/tokyo_metro/app/renderer/real_time_infos/link_to_railway_line_page/train_location.rb
@@ -1,2 +1,2 @@
-class TokyoMetro::App::Renderer::RealTimeInfos::LinkToRailwayLinePage::TrainLocation < TokyoMetro::App::Renderer::Concerns::Link::ToRailwayLinePage::RealTimeInfos::TrainOperation
+class TokyoMetro::App::Renderer::RealTimeInfos::LinkToRailwayLinePage::TrainLocation < TokyoMetro::App::Renderer::Concerns::Link::ToRailwayLinePage::RealTimeInfos::TrainLocation
 end
