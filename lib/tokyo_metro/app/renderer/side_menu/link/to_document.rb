3a85714c1bb88cda160eb33f08351d02d75b7c4d
Shu Fujita <osorubeki.fujita@gmail.com>
2015-06-20 13:59:16 +0900

    20150620_135907 Bug fix - TokyoMetro::App::Renderer::SideMenu::Link::ToDocument

--- lib/tokyo_metro/app/renderer/side_menu/link/to_document.rb
+++ lib/tokyo_metro/app/renderer/side_menu/link/to_document.rb
@@ -9,15 +9,20 @@ class TokyoMetro::App::Renderer::SideMenu::Link::ToDocument < TokyoMetro::App::R
       icon_name: icon_name ,
       class_name_of_link: :link ,
       class_name_of_div_domain: :link_to_document ,
-      open_another_window: link_for_page_on_the_same_controller? ,
+      open_another_window: nil ,
       size: :small
     )
+    @open_another_window = open_another_window? 
   end
 
   alias :__link_for_page_on_the_same_category? :link_for_page_on_the_same_category?
 
   def link_for_page_on_the_same_category?
     __link_for_page_on_the_same_category? and link_for_page_of_the_same_action?
+  end
+
+  def open_another_window?
+    link_for_page_on_the_same_controller?
   end
 
   def self.development_document( request )
