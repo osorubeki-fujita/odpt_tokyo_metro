f437bc9e917bac492ad9d48633d00bc65c856476
Shu Fujita <osorubeki.fujita@gmail.com>
2015-06-27 15:42:54 +0900

    20150627_154249 Bug fix - TokyoMetro::Required::All::Factory::Get::Api::MetaClass

--- lib/tokyo_metro/required/all/factory/get/api/meta_class.rb
+++ lib/tokyo_metro/required/all/factory/get/api/meta_class.rb
@@ -1,7 +1,7 @@
 class TokyoMetro::Required::All::Factory::Get::Api::MetaClass < TokyoMetro::Required
 
   def self.other_files
-    Fundamental.files + ::File.join( top_file , "find" )
+    Fundamental.files + [ ::File.join( top_file , "find" ) ]
   end
 
 end
