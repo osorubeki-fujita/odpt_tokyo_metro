53de4b036baf4e678c8bf01c03adeccc86154012
Shu Fujita <osorubeki.fujita@gmail.com>
2015-06-28 15:58:55 +0900

    20150628_155836 Bug fix - TokyoMetro::Factory::Get::Api::MetaClass::Fundamental::Generator::NewArray::Common

--- lib/tokyo_metro/factory/get/api/meta_class/fundamental/generator/new_array/common.rb
+++ lib/tokyo_metro/factory/get/api/meta_class/fundamental/generator/new_array/common.rb
@@ -1,16 +1,16 @@
 class TokyoMetro::Factory::Get::Api::MetaClass::Fundamental::Generator::NewArray::Common
-  
-  private
-  
-  def check_validity_of_ary
-    raise unless @ary.all? { | item | item.instance_of?( ::Hash ) }
-  end
 
   def process
     _info_class = info_class
     list_class.new( ary.map { | item |
       _info_class.generate_from_hash( item )
     } )
+  end
+  
+  private
+  
+  def check_validity_of_ary
+    raise unless @ary.all? { | item | item.instance_of?( ::Hash ) }
   end
   
 end
