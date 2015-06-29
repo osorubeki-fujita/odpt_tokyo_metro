c5c7ae798610c59f1b13d38af4b22161501876a8
Shu Fujita <osorubeki.fujita@gmail.com>
2015-06-29 21:07:44 +0900

    20150629_210734 Bug fix - TokyoMetro::Factory::Get::Api::MetaClass::Fundamental::Generator::NewArray::Find

--- lib/tokyo_metro/factory/get/api/meta_class/fundamental/generator/new_array/find.rb
+++ lib/tokyo_metro/factory/get/api/meta_class/fundamental/generator/new_array/find.rb
@@ -22,18 +22,22 @@ class TokyoMetro::Factory::Get::Api::MetaClass::Fundamental::Generator::NewArray
     raise unless @ary.length == 1
   end
 
+  def set_namespace
+    @namespace = ::TokyoMetro::Api.const_get( namespace_basename )
+  end
+  
+  def namespace_basename
+    _rdf_type_from_api = rdf_type_from_api
+
+    _namespace_basename = namespace_basenames.find { | item | ::TokyoMetro::Api.const_get( item ).rdf_type == _rdf_type_from_api }
+    raise unless _namespace_basename.present?
+    _namespace_basename
+  end
+
   def rdf_type_from_api
     rdf = @ary.first[ "@type" ]
     raise unless rdf.present?
     rdf
-  end
-
-  def set_namespace
-    _rdf_type_from_api = rdf_type_from_api
-
-    namespace_basename = namespace_basenames.find { | item | ::TokyoMetro::Api.const_get( item ).rdf_type == _rdf_type_from_api }
-    raise unless namespace_basename.present?
-    @namespace = ::TokyoMetro::Api.const_get( namespace_basename )
   end
 
   def namespace_basenames
