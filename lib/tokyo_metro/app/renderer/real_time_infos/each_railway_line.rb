788078d2fb52f51bf3c73ac4eafec202ee3cb8e8
Shu Fujita <osorubeki.fujita@gmail.com>
2015-06-30 18:57:43 +0900

    20150630_185739 Bundle Update

--- lib/tokyo_metro/app/renderer/real_time_infos/each_railway_line.rb
+++ lib/tokyo_metro/app/renderer/real_time_infos/each_railway_line.rb
@@ -146,21 +146,3 @@ SocketError - getaddrinfo: ���̂悤�ȃz�X�g�͕s���ł��B 
   actionpack (4.2.1) lib/action_controller/metal/implicit_render.rb:4:in `send_action'
 
 
-
-2015-06-25 08:17:56 +0900
-
-[
-  {
-    "@context"=>"http://vocab.tokyometroapp.jp/context_odpt_TrainInformation.json",
-    "@id"=>"urn:ucode:_00001C000000000000010000030C3BE5",
-    "dc:date"=>"2015-06-25T08:15:02+09:00",
-    "dct:valid"=>"2015-06-25T08:20:02+09:00",
-    "odpt:operator"=>"odpt.Operator:TokyoMetro",
-    "odpt:railway"=>"odpt.Railway:TokyoMetro.Hanzomon",
-    "odpt:timeOfOrigin"=>"2015-06-25T07:14:00+09:00",
-    "odpt:trainInformationStatus"=>"ダイヤ乱れ",
-    "odpt:trainInformationText"=>"6時35分頃、東急田園都市線 たまプラーザ駅で人身事故のため、ダイヤが乱れています。この影響で女性専用車両を中止しています。只今、東京メトロ線、都営地下鉄線、ＪＲ線、東武線に振替輸送を実施しています。詳しくは、駅係員にお尋ねください。",
-    "@type"=>"odpt:TrainInformation"
-  }
-]
-[]
