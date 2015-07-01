d172001d464b0c94eb320f8dd2a0edf95bd3c8fc
Shu Fujita <osorubeki.fujita@gmail.com>
2015-07-01 19:06:01 +0900

    20150701_190549 Update - TokyoMetro::Factory::Convert::Customize::Api::TrainLocation::ChiyodaBranchLine::Info

--- lib/tokyo_metro/factory/convert/customize/api/train_location/chiyoda_branch_line/info.rb
+++ lib/tokyo_metro/factory/convert/customize/api/train_location/chiyoda_branch_line/info.rb
@@ -1,2 +1,28 @@
 class TokyoMetro::Factory::Convert::Customize::Api::TrainLocation::ChiyodaBranchLine::Info < TokyoMetro::Factory::Convert::Customize::Api::MetaClass::TrainInfos::ChiyodaBranchLine::Info
+
+  def process
+    super
+    convert_invalid_station_name_of_chiyoda_main_line_train
+  end
+
+  private
+
+  def convert_invalid_station_name_of_chiyoda_main_line_train
+    if invalid_chiyoda_main_line_train_terminating_at_kita_ayase?
+      regexp = /(?<=TokyoMetro\.Chiyoda\.)Kita(?=Ayase)/
+      puts "=" * 4 + " " + "invalid_chiyoda_main_line_train_terminating_at_kita_ayase"
+      puts object.id_urn
+      puts object.same_as
+      object.instance_variable_set( :@terminal_station , object.terminal_station.gsub( regexp , "" ) )
+    end
+  end
+
+  def invalid_chiyoda_main_line_train_terminating_at_kita_ayase?
+    object.chiyoda_line? and starting_on_yoyogi_uehara_side? and object.terminating_at_kita_ayase_on_chiyoda_main_line?
+  end
+
+  def starting_on_yoyogi_uehara_side?
+    object.starting_at_yoyogi_uehara? or object.starting_on_odakyu_line? or object.starting_on_hakone_tozan_line?
+  end
+
 end
