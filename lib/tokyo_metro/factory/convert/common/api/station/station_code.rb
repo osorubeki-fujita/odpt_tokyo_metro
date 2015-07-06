16c7f1d9845ee84c11f42459e43dc15a013191b9
Shu Fujita <osorubeki.fujita@gmail.com>
2015-07-06 21:15:43 +0900

    20150706_211538 Bug fix - TokyoMetro::Factory::Convert::Common::Api::Station::StationCode

--- lib/tokyo_metro/factory/convert/common/api/station/station_code.rb
+++ lib/tokyo_metro/factory/convert/common/api/station/station_code.rb
@@ -1,14 +1,14 @@
 class TokyoMetro::Factory::Convert::Common::Api::Station::StationCode < TokyoMetro::Factory::Convert::Common::Api::MetaClass::Fundamental::Normal
 
-  def initialize( obj , station_same_as , station_code_new )
-    super( obj )
+  def initialize( object , station_same_as , station_code_new )
+    super( object )
     @station_same_as = station_same_as
     @station_code_new = station_code_new
   end
 
   def process
-    if obj.same_as == @station_same_as
-      obj.instance_variable_set( :@station_code , "m06" )
+    if object.same_as == @station_same_as
+      object.instance_variable_set( :@station_code , "m06" )
     end
   end
 
