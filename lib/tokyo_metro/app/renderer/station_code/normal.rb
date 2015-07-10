159d874481740c208b1e8e414aa8dcc643307dfc
Shu Fujita <osorubeki.fujita@gmail.com>
2015-07-10 20:09:17 +0900

    20150710_200923 Update - TokyoMetro::App::Renderer::StationCode::Normal

--- lib/tokyo_metro/app/renderer/station_code/normal.rb
+++ lib/tokyo_metro/app/renderer/station_code/normal.rb
@@ -7,21 +7,24 @@ class TokyoMetro::App::Renderer::StationCode::Normal < TokyoMetro::Factory::Deco
     if @station_infos.all?( &:at_ayase? ) or @station_infos.all?( &:at_nakano_sakaue? )
       @station_infos = [ @station_infos.first ]
     end
-    @first_info = first_info
+
+    if first_info.present?
+      ary = ::Array.new
+      ary << first_info
+      other_station_infos = @station_infos.select { | info | info.id != first_info.id }
+      if other_station_infos.present?
+        ary << other_station_infos
+      end
+
+      @station_infos = ary.flatten
+    end
   end
 
   def render
     h.render inline: <<-HAML , type: :haml , locals: h_locals
 %div{ class: :station_codes }<
-  - if first_info.present?
-    = first_info.decorate.render_each_station_code_image_tag
-    - other_station_infos = station_infos.select { | info | info.id != first_info.id }
-    - if other_station_infos.present?
-      - other_station_infos.each do | info |
-        = info.decorate.render_each_station_code_image_tag
-  - else
-    - station_infos.each do | info |
-      = info.decorate.render_each_station_code_image_tag
+  - station_infos.each do | info |
+    = info.decorate.render_each_station_code_image_tag
     HAML
   end
 
@@ -29,8 +32,7 @@ class TokyoMetro::App::Renderer::StationCode::Normal < TokyoMetro::Factory::Deco
 
   def h_locals
     super.merge({
-      station_infos: @station_infos ,
-      first_info: @first_info
+      station_infos: @station_infos
     })
   end
 
