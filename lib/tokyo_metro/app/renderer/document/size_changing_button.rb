809fa84f7fc94e6497dc47f7ac6be63904993ba4
Shu Fujita <osorubeki.fujita@gmail.com>
2015-07-10 13:44:06 +0900

    20150710_134341 Update - list of Font Awesome icons / Add - Renderer of size changing buttons

---
+++ lib/tokyo_metro/app/renderer/document/size_changing_button.rb
@@ -0,0 +1,25 @@
+class TokyoMetro::App::Renderer::Document::SizeChangingButton < ::Factory::Decorate::MetaClass
+
+  def render
+    h.render inline: <<-HAML , type: :haml , locals: h_locals
+%ul{ class: :size_changing_buttons }
+  - icon_names.each do | icon_name |
+    %li{ class: [ :size_changing_button , icon_name ] }
+      = ::TokyoMetro::App::Renderer::Icon.send( icon_name , nil , 1 ).render
+    HAML
+  end
+
+  private
+
+  def h_locals
+    super.merge({
+      icon_names: [
+        :display_all ,
+        :display ,
+        :minimize ,
+        :minimize_all
+      ]
+    })
+  end
+
+end
