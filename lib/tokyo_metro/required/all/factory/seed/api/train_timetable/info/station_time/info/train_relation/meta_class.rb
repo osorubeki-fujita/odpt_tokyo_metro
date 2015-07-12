0307850edf9f019482ba8a44a740cf30bef5c5bc
Shu Fujita <osorubeki.fujita@gmail.com>
2015-07-13 01:47:31 +0900

    20150713_014727 Update - TokyoMetro::Required::All::Factory::Seed::Api::TrainTimetable::Info::StationTime::Info::TrainRelation::MetaClass

--- lib/tokyo_metro/required/all/factory/seed/api/train_timetable/info/station_time/info/train_relation/meta_class.rb
+++ lib/tokyo_metro/required/all/factory/seed/api/train_timetable/info/station_time/info/train_relation/meta_class.rb
@@ -1,7 +1,7 @@
 class TokyoMetro::Required::All::Factory::Seed::Api::TrainTimetable::Info::StationTime::Info::TrainRelation::MetaClass < TokyoMetro::Required
 
   def self.other_files
-    [ "optional_infos" , "list" , "info" ].map {} | filename |
+    [ "optional_infos" , "list" , "info" ].map { | filename |
       ::File.join( top_file , filename )
     }
   end
