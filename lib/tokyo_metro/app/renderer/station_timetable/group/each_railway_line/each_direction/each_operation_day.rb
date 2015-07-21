75fcc8a09e9e1754c4c1c8990ae6d8a57432f854
Shu Fujita <osorubeki.fujita@gmail.com>
2015-07-22 01:19:40 +0900

    20150722_011936 Bug fix - TokyoMetro::App::Renderer::StationTimetable::Group::EachRailwayLine::EachRailwayDirection::EachOperationDay

--- lib/tokyo_metro/app/renderer/station_timetable/group/each_railway_line/each_direction/each_operation_day.rb
+++ lib/tokyo_metro/app/renderer/station_timetable/group/each_railway_line/each_direction/each_operation_day.rb
@@ -3,14 +3,14 @@ class TokyoMetro::App::Renderer::StationTimetable::Group::EachRailwayLine::EachR
 
   # Constructor
   # @param station_train_times_of_a_direction_and_an_operation_day [Array <TrainTime>] 各列車の情報（路線・方面・運行日別）のリスト
-  # @param railway_line [RailwayLine] 路線のインスタンス
+  # @param railway_line_info [RailwayLine] 路線のインスタンス
   # @param direction [Railway::Direction] 方面のインスタンス
   # @param operation_day [OperationDay] 運行日のインスタンス
   # @param station [Station] 駅のインスタンス（駅に複数の路線が乗り入れている場合は、代表する路線のインスタンス）
-  def initialize( request , station_info , railway_line , railway_direction , operation_day , station_train_times )
+  def initialize( request , station_info , railway_line_info , railway_direction , operation_day , station_train_times )
     super( request )
     @station_info = station_info
-    @railway_line = railway_line
+    @railway_line_info = railway_line_info
     @railway_direction = railway_direction
     @operation_day = operation_day
 
@@ -31,10 +31,10 @@ class TokyoMetro::App::Renderer::StationTimetable::Group::EachRailwayLine::EachR
 
   def render_header
     h.render inline: <<-HAML , type: :haml , locals: h_locals
-%thead{ class: railway_line.css_class }
+%thead{ class: railway_line_info.css_class }
   %tr
     %td{ colspan: 2 , class: :top_header }
-      = railway_line.decorate.render_in_station_timetable_header
+      = railway_line_info.decorate.render_in_station_timetable_header
       %div{ class: :main }
         = operation_day.decorate.render_in_station_timetable_header
         = railway_direction.decorate.render_in_station_timetable_header
@@ -47,11 +47,11 @@ class TokyoMetro::App::Renderer::StationTimetable::Group::EachRailwayLine::EachR
   private
 
     # @note
-    #   station （駅のインスタンス）に保持されている路線の情報が railway_line （路線のインスタンス）と異なる場合は、
-    #   railway_line を優先し、station と同名の駅に乗り入れている路線の中から railway_line と矛盾しない駅のインスタンスを取得する。
+    #   station （駅のインスタンス）に保持されている路線の情報が railway_line_info （路線のインスタンス）と異なる場合は、
+    #   railway_line_info を優先し、station と同名の駅に乗り入れている路線の中から railway_line_info と矛盾しない駅のインスタンスを取得する。
   def set_valid_station_info
-    unless @station_info.railway_line_info_id == @railway_line.id
-      @station_info = @station_info.station_infos_including_other_railway_line_infos.find_by( railway_line_info_id: @railway_line.id )
+    unless @station_info.railway_line_info_id == @railway_line_info.id
+      @station_info = @station_info.station_infos_including_other_railway_line_infos.find_by( railway_line_info_id: @railway_line_info.id )
     end
   end
 
@@ -59,7 +59,7 @@ class TokyoMetro::App::Renderer::StationTimetable::Group::EachRailwayLine::EachR
     super.merge({
       this: self ,
       station_info: @station_info ,
-      railway_line: @railway_line ,
+      railway_line_info: @railway_line_info ,
       railway_direction: @railway_direction ,
       operation_day: @operation_day ,
       station_train_times: @station_train_times
