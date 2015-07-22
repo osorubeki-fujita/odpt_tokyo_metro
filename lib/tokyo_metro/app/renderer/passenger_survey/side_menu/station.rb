cad29a54fdf5c163ff6725059a7e1b30d8592e53
Shu Fujita <osorubeki.fujita@gmail.com>
2015-07-22 20:21:12 +0900

    20150722_202109 Bug fix -  TokyoMetro::App::Renderer::PassengerSurvey::SideMenu::Station

--- lib/tokyo_metro/app/renderer/passenger_survey/side_menu/station.rb
+++ lib/tokyo_metro/app/renderer/passenger_survey/side_menu/station.rb
@@ -2,23 +2,23 @@ class TokyoMetro::App::Renderer::PassengerSurvey::SideMenu::Station < TokyoMetro
 
   def initialize( request , station_info )
     super( request )
+    @railway_line_infos =  station_info.railway_line_infos_of_tokyo_metro
 
     if station_info.at_kita_ayase? or station_info.between_honancho_and_nakano_shimbashi?
       @prior_railway_line_info_ids = station_info.railway_line.main_railway_line_info.id
     else
-      railway_line_infos = station_info.railway_line_infos_of_tokyo_metro
-      @prior_railway_line_info_ids = railway_line_infos.pluck( :railway_line_info_id ).uniq
+      @prior_railway_line_info_ids = @railway_line_infos.pluck( :railway_line_info_id ).uniq
     end
   end
 
   private
 
   def railway_lines_prior
-    @all_railway_lines.where( id: @prior_railway_line_info_ids )
+    @railway_line_infos.where( id: @prior_railway_line_info_ids )
   end
 
   def railway_lines_not_prior
-    @all_railway_lines.where.not( id: @prior_railway_line_info_ids )
+    @railway_line_infos.where.not( id: @prior_railway_line_info_ids )
   end
 
   def proc_for_links_to_railway_line_pages
