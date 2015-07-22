fe3d143ee1b515b8f51340e82a68a5de598db82a
Shu Fujita <osorubeki.fujita@gmail.com>
2015-07-22 18:43:43 +0900

    20150722_184339 Bug fix - TokyoMetro::Factory::Seed::Reference::Operator

--- lib/tokyo_metro/factory/seed/reference/operator.rb
+++ lib/tokyo_metro/factory/seed/reference/operator.rb
@@ -2,7 +2,7 @@ module TokyoMetro::Factory::Seed::Reference::Operator
 
   private
 
-  def operator_info_of?( whole = nil , search_by: @info.operator )
+  def operator_info_in_db( whole = nil , search_by: @info.operator )
     if whole.present?
       whole.find_by_same_as( search_by )
     else
@@ -11,9 +11,9 @@ module TokyoMetro::Factory::Seed::Reference::Operator
   end
 
   def operator_info_id( whole = nil , search_by: @info.operator )
-    _operator_info_of? = operator_info_of?( whole , search_by: search_by )
-    raise "Error: \"#{ search_by }\" does not exist in the database." if _operator_info_of?.nil?
-    _operator_info_of?.id
+    _operator_info_in_db = operator_info_in_db( whole , search_by: search_by )
+    raise "Error: \"#{ search_by }\" does not exist in the database." if _operator_info_in_db.nil?
+    _operator_info_in_db.id
   end
 
 end
