47bc6d4e686ec405cd70da2bf7b06cd434fa27fe
Shu Fujita <osorubeki.fujita@gmail.com>
2015-07-23 19:37:05 +0900

    20150723_193701 Update - TokyoMetro::App::Renderer::StationMatrix

--- lib/tokyo_metro/app/renderer/station_matrix.rb
+++ lib/tokyo_metro/app/renderer/station_matrix.rb
@@ -1,12 +1,13 @@
 # 駅一覧表（路線別）を作成するためのクラス
 class TokyoMetro::App::Renderer::StationMatrix < TokyoMetro::Factory::Decorate::MetaClass
 
-  def initialize( request , railway_lines , make_link_to_railway_line: false , type_of_link_to_station: nil , set_anchor: false )
+  def initialize( request , railway_lines , make_link_to_railway_line: false , type_of_link_to_station: nil , set_anchor: false , controller_of_linked_page_from_railway_line: nil )
     super( request )
     @railway_lines = railway_lines.except_for_branch_lines
     @make_link_to_railway_line = make_link_to_railway_line
     @type_of_link_to_station = type_of_link_to_station
     @set_anchor = set_anchor
+    @controller_of_linked_page_from_railway_line = controller_of_linked_page_from_railway_line
   end
 
   def render
@@ -14,7 +15,7 @@ class TokyoMetro::App::Renderer::StationMatrix < TokyoMetro::Factory::Decorate::
 = ::TokyoMetro::App::Renderer::Concerns::Header::Selection.station_from_railway_line( request ).render
 %div{ id: :station_matrixes }
   - railway_lines.each do | railway_line |
-    = railway_line.decorate.matrix.on( request ).render_with_links_to_stations( make_link_to_railway_line , type_of_link_to_station , set_anchor )
+    = railway_line.decorate.matrix.on( request ).render_with_links_to_stations( make_link_to_railway_line , type_of_link_to_station , set_anchor , controller_of_linked_page_from_railway_line: controller_of_linked_page_from_railway_line )
     HAML
   end
 
@@ -25,7 +26,8 @@ class TokyoMetro::App::Renderer::StationMatrix < TokyoMetro::Factory::Decorate::
       railway_lines: @railway_lines ,
       make_link_to_railway_line: @make_link_to_railway_line ,
       type_of_link_to_station: @type_of_link_to_station ,
-      set_anchor: @set_anchor
+      set_anchor: @set_anchor ,
+      controller_of_linked_page_from_railway_line: controller_of_linked_page_from_railway_line
     })
   end
 
