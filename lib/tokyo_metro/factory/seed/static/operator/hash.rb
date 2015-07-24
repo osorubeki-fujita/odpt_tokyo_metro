d368f8d8ec6012ef91b66abb14c3d324cbbe0cc6
Shu Fujita <osorubeki.fujita@gmail.com>
2015-07-25 02:09:34 +0900

    20150725_020931 Bug fix - TokyoMetro::Factory::Seed::Static::Operator::Hash

--- lib/tokyo_metro/factory/seed/static/operator/hash.rb
+++ lib/tokyo_metro/factory/seed/static/operator/hash.rb
@@ -19,7 +19,7 @@ class TokyoMetro::Factory::Seed::Static::Operator::Hash < TokyoMetro::Factory::S
     self.class.db_instance_class_of_code.find_or_create_by(
       info_id: undefined_instance.id
       color: "\#999999"
-      railway_line_code_shape: none ,
+      railway_line_code_shape: "none" ,
       numbering: false
     )
 
