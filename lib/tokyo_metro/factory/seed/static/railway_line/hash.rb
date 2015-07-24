02fc66872c6d5edc80581e7354d278103f6a4bdc
Shu Fujita <osorubeki.fujita@gmail.com>
2015-07-25 05:15:29 +0900

    20150725_051525 Update - TokyoMetro::Factory::Seed::Static::RailwayLine::Hash

--- lib/tokyo_metro/factory/seed/static/railway_line/hash.rb
+++ lib/tokyo_metro/factory/seed/static/railway_line/hash.rb
@@ -9,7 +9,10 @@ class TokyoMetro::Factory::Seed::Static::RailwayLine::Hash < TokyoMetro::Factory
       same_as: "odpt.Railway:Undefined" ,
       name_ja: "未定義" ,
       name_en: "Undefined" ,
-      operator_info_id: ::Operator::Info.find_by( same_as: "odpt.Operator:Undefined" ).id
+      operator_info_id: ::Operator::Info.find_by( same_as: "odpt.Operator:Undefined" ).id ,
+      color: "\#999999" ,
+      index_in_operator: 1 ,
+      name_codes: "NG"
     )
   end
 
