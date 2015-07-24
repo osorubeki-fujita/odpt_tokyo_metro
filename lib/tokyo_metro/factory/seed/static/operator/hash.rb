518122552d4c890b49416f33b9fc443024b4ac46
Shu Fujita <osorubeki.fujita@gmail.com>
2015-07-25 02:05:35 +0900

    20150725_020525 Update - TokyoMetro::Factory::Seed::Static::Operator::Hash (settings of undefined operator)

--- lib/tokyo_metro/factory/seed/static/operator/hash.rb
+++ lib/tokyo_metro/factory/seed/static/operator/hash.rb
@@ -9,11 +9,20 @@ class TokyoMetro::Factory::Seed::Static::Operator::Hash < TokyoMetro::Factory::S
   end
 
   def seed_instance_for_escaping_undefined
-    self.class.db_instance_class.find_or_create_by(
+    undefined_instance = self.class.db_instance_class.find_or_create_by(
       same_as: "odpt.Operator:Undefined" ,
       name_ja: "未定義" ,
-      name_en: "Undefined"
+      name_en: "Undefined" ,
+      index: 9999
     )
+
+    self.class.db_instance_class_of_code.find_or_create_by(
+      info_id: undefined_instance.id
+      color: "\#999999"
+      railway_line_code_shape: none ,
+      numbering: false
+    )
+
   end
 
 end
