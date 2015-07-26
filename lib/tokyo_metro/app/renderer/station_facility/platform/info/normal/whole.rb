a230f69e7a495d2197105a9d8eb18e734ff7f225
Shu Fujita <osorubeki.fujita@gmail.com>
2015-07-26 23:31:26 +0900

    20150726_233119 Update - TokyoMetro::App::Renderer::StationFacility::Platform::Info::Normal::Whole

--- lib/tokyo_metro/app/renderer/station_facility/platform/info/normal/whole.rb
+++ lib/tokyo_metro/app/renderer/station_facility/platform/info/normal/whole.rb
@@ -7,7 +7,7 @@ class TokyoMetro::App::Renderer::StationFacility::Platform::Info::Normal::Whole 
     h.render inline: <<-HAML , type: :haml , locals: { this: self }
 %li{ class: [ "tab_for_" + this.tab_name.to_s , :platform_info_tab , this.railway_line_info.css_class , :clearfix ] }<
   %div{ class: [ :railway_line_name , :clearfix ] , onclick: raw( "changeStationFacilityPlatformInfoTabByPageLink('" + this.tab_name.to_s + "' , true ) ; return false ; " ) }
-    = this.railway_line_info.decorate.code.render( small: true )
+    = this.railway_line_info.decorate.code_domain.render( small: true )
     = this.render_railway_line_text_name
     HAML
   end
