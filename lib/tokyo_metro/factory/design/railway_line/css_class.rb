e58a8ae9961c117c829742eddb4b06f95598ded3
Shu Fujita <osorubeki.fujita@gmail.com>
2015-07-27 01:11:55 +0900

    20150727_011151 Update - TokyoMetro::Factory::Design::RailwayLine::CssClass

--- lib/tokyo_metro/factory/design/railway_line/css_class.rb
+++ lib/tokyo_metro/factory/design/railway_line/css_class.rb
@@ -1,7 +1,8 @@
 class TokyoMetro::Factory::Design::RailwayLine::CssClass < OdptCommon::Factory::Design::RailwayLine::CssClass
 
-  def to_s
-    str = super()
+  def to_s( i = 0 )
+    str = super(i)
+
     if @object.marunouchi_branch_line? or @object.chiyoda_branch_line?
       str = str.gsub( /\Atokyo_metro_/ , "" )
     end
