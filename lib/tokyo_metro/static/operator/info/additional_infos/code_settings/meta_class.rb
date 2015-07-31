95115dd5af758cfdd8b9e0efe3cfc0b24e51843c
Shu Fujita <osorubeki.fujita@gmail.com>
2015-08-01 06:59:06 +0900

    20150801_065900 Bug fix - TokyoMetro::Static::Operator::Info::AdditionalInfos::CodeSettings::MetaClass

--- lib/tokyo_metro/static/operator/info/additional_infos/code_settings/meta_class.rb
+++ lib/tokyo_metro/static/operator/info/additional_infos/code_settings/meta_class.rb
@@ -1,5 +1,8 @@
 class TokyoMetro::Static::Operator::Info::AdditionalInfos::CodeSettings::MetaClass
 
+  include ::TokyoMetro::Modules::ToFactory::Common::Generate::Info
+  include ::TokyoMetro::ClassNameLibrary::Static::Operator
+
   def initialize( shape , stroke_width , text_weight , text_size )
     @shape = shape
     @stroke_width = stroke_width
