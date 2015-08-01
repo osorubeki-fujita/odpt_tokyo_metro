b711747350da586f79ab977efe421712b6bac1ee
Shu Fujita <osorubeki.fujita@gmail.com>
2015-08-02 05:56:41 +0900

    20150802_055637 Update - TokyoMetro::Static::Operator::Info::Name

--- lib/tokyo_metro/static/operator/info/name.rb
+++ lib/tokyo_metro/static/operator/info/name.rb
@@ -7,10 +7,11 @@ class TokyoMetro::Static::Operator::Info::Name
   # @param ja_short [String] 事業者の名称（日本語、略称・表示用）
   # @param en [::Array <::String>] 事業者の名称（ローマ字表記、正式名）
   # @param en_short [String] 事業者の名称（ローマ字表記、略称・表示用）
-  def initialize( ja , ja_short , hira , en , en_short )
+  def initialize( ja , ja_short , hira , hira_short , en , en_short )
     @ja = ja
     @ja_short = ja_short
     @hira = hira
+    @hira_short = hira_short
     @en = en
     @en_short = en_short
   end
@@ -18,6 +19,7 @@ class TokyoMetro::Static::Operator::Info::Name
   attr_reader :ja
   attr_reader :ja_short
   attr_reader :hira
+  attr_reader :hira_short
   attr_reader :en
   attr_reader :en_short
 
@@ -26,7 +28,7 @@ class TokyoMetro::Static::Operator::Info::Name
   end
 
   def to_a
-    [ @ja , @ja_short , @hira , @en , @en_short ]
+    [ @ja , @ja_short , @hira , @hira_short , @en , @en_short ]
   end
 
   def to_h
@@ -34,6 +36,7 @@ class TokyoMetro::Static::Operator::Info::Name
       ja: @ja ,
       ja_short: @ja_short ,
       hira: @hira ,
+      hira_short: @hira_short ,
       en: @en ,
       en_short: @en_short
     }
