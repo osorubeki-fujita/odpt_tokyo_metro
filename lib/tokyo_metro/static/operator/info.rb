c0fcff17f5bec5dd051a8652bbf21233c20e1dd6
Shu Fujita <osorubeki.fujita@gmail.com>
2015-08-02 06:49:19 +0900

    20150802_064915 Update - TokyoMetro::Static::Operator::Info

--- lib/tokyo_metro/static/operator/info.rb
+++ lib/tokyo_metro/static/operator/info.rb
@@ -18,6 +18,10 @@ class TokyoMetro::Static::Operator::Info < TokyoMetro::Static::Fundamental::Info
 
   include ::OdptCommon::Modules::MethodMissing::Decision::Common::Operator
 
+  def self.instance_variable_names
+    [ :same_as , :name , :index , :additional_infos , :twitter_account_info ]
+  end
+
 # @!group Constructor
 
   # Constructor
