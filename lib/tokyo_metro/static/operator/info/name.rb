34aaba33faddf62cb954d69e74f4f76dfa8c0a6a
Shu Fujita <osorubeki.fujita@gmail.com>
2015-08-02 06:12:40 +0900

    20150802_061228 Update - TokyoMetro::Static::Operator::Info::Name

--- lib/tokyo_metro/static/operator/info/name.rb
+++ lib/tokyo_metro/static/operator/info/name.rb
@@ -3,24 +3,30 @@ class TokyoMetro::Static::Operator::Info::Name
   include ::TokyoMetro::Modules::ToFactory::Common::Generate::Info
   include ::TokyoMetro::ClassNameLibrary::Static::Operator
 
+  def self.variable_names
+    [ :ja , :hira , :en , :ja_short , :hira_short , :en_short ]
+  end
+
   # @param ja [::Array <::String>] 事業者の名称（日本語、正式名）
   # @param ja_short [String] 事業者の名称（日本語、略称・表示用）
   # @param en [::Array <::String>] 事業者の名称（ローマ字表記、正式名）
   # @param en_short [String] 事業者の名称（ローマ字表記、略称・表示用）
-  def initialize( ja , ja_short , hira , hira_short , en , en_short )
+  def initialize( ja , hira , en , ja_short , hira_short , en_short )
     @ja = ja
-    @ja_short = ja_short
     @hira = hira
-    @hira_short = hira_short
     @en = en
+
+    @ja_short = ja_short
+    @hira_short = hira_short
     @en_short = en_short
   end
 
   attr_reader :ja
-  attr_reader :ja_short
   attr_reader :hira
-  attr_reader :hira_short
   attr_reader :en
+
+  attr_reader :ja_short
+  attr_reader :hira_short
   attr_reader :en_short
 
   def self.factory_for_generating_instance_of_this_class
@@ -28,16 +34,16 @@ class TokyoMetro::Static::Operator::Info::Name
   end
 
   def to_a
-    [ @ja , @ja_short , @hira , @hira_short , @en , @en_short ]
+    [ @ja , @hira , @en , @ja_short , @hira_short , @en_short ]
   end
 
   def to_h
     {
       ja: @ja ,
-      ja_short: @ja_short ,
       hira: @hira ,
-      hira_short: @hira_short ,
       en: @en ,
+      ja_short: @ja_short ,
+      hira_short: @hira_short ,
       en_short: @en_short
     }
   end
